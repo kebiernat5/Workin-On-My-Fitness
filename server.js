14c2ecb021d20ec2a6870a71ed293058aa9217f7
Kate Biernat <kebiernat5@gmail.com>
2021-04-17 01:39:13 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
 { 
-useNewUrlParser: true 
+useNewUrlParser: true, 
 useUnifiedTopology: true,
 useCreateIndex: true,
 useFindAndModify: false
